69750cf2c976046346e422b3636bf3eaea452b91
Ajmayen Fayek <ajmayenfayek999999@gmail.com>
2026-02-02 22:37:16 +0600

    First Commit

---
+++ src/main/java/com/ajmayen/smartdepo/model/Category.java
@@ -0,0 +1,27 @@
+package com.ajmayen.smartdepo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String name;
+
+    private LocalDateTime createdAt =  LocalDateTime.now();
+
+
+}
---
+++ src/test/java/com/ajmayen/smartdepo/SmartDepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajmayen.smartdepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
